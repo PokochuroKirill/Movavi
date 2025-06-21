34ad768ddb1b6183b49e00d485b0b62f6a682ab1
Laywood <pomilli911@gmail.com>
2025-06-21 19:40:20 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import CommunityPostDetailPage from '@/pages/CommunityPostDetailPage';
 // добавляем импорт AboutPage и AdminPage
 import AboutPage from '@/pages/AboutPage';
 import AdminPage from '@/pages/AdminPage';
-import VerificationTermsPage from '@/pages/VerificationTerms';
+import VerificationTermsPage from '@/pages/VerificationPage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
